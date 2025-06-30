d27de6216e7703c45f5b839f13edd7720b692c8e
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2025-06-30 20:07:50 +0100

    Minor improvement on the syscall declaration

--- src/lib.rs
+++ src/lib.rs
@@ -86,11 +86,13 @@ pub fn merlin_syscall(attr: TokenStream, item: TokenStream) -> TokenStream {
         #(#fn_attrs)*
         #[allow(dead_code)]
         #[allow(unused)]
+        #[allow(non_snake_case)]
         #fn_vis extern "C" fn #fn_name(#fn_args) #fn_body
 
         #[link_section = ".merlin_syscall_entries"]
         #[used]
         #[no_mangle]
+        #[allow(non_upper_case_globals)]
         pub static #entry_ident:  crate::kernel::syscalls::syscall_macro::StaticSysCallEntry =
             crate::kernel::syscalls::syscall_macro::StaticSysCallEntry {
                 id: #syscall_id,
